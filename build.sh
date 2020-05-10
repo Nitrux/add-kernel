290e618bb43fd6dfa1b76a89edc1b00b455212aa
luis-lavaire <llavaire01@gmail.com>
2020-05-10 17:57:30 -0600

    test if it works.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+
+(
+	mkdir files
+	cd files
+
+	wget \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.21/linux-headers-5.4.21-050421_5.4.21-050421.202002191431_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.21/linux-headers-5.4.21-050421-generic_5.4.21-050421.202002191431_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.21/linux-image-unsigned-5.4.21-050421-generic_5.4.21-050421.202002191431_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.21/linux-modules-5.4.21-050421-generic_5.4.21-050421.202002191431_amd64.deb"
+)
+
+
+wget -qO nru https://raw.githubusercontent.com/Nitrux/nitrux-repository-util/master/nitrux-repository-util.sh
+bash nru files/*
