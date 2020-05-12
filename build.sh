84d2fcb79ce74156980d1df10347d32676e7d07e
luis-lavaire <llavaire01@gmail.com>
2020-05-12 13:46:30 -0600

    fixed nru. pass the '-q' flag to wget.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 	mkdir files
 	cd files
 
-	wget \
+	wget -q \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.21/linux-headers-5.4.21-050421_5.4.21-050421.202002191431_all.deb" \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.21/linux-headers-5.4.21-050421-generic_5.4.21-050421.202002191431_amd64.deb" \
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.21/linux-image-unsigned-5.4.21-050421-generic_5.4.21-050421.202002191431_amd64.deb" \
