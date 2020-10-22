6668eb216dca80bdf4ee1619dd208b4cad87e7f1
Uri Herrera <hello@uriherrera.com>
2020-10-22 15:46:02 -0500

    add latest kernel to repo

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-headers-5.4.72-050472-generic_5.4.72-050472.202010170535_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-headers-5.4.72-050472_5.4.72-050472.202010170535_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-image-unsigned-5.4.72-050472-generic_5.4.72-050472.202010170535_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-modules-5.4.72-050472-generic_5.4.72-050472.202010170535_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.1/amd64/linux-headers-5.9.1-050901-generic_5.9.1-050901.202010170731_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.1/amd64/linux-headers-5.9.1-050901_5.9.1-050901.202010170731_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.1/amd64/linux-image-unsigned-5.9.1-050901-generic_5.9.1-050901.202010170731_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.1/amd64/linux-modules-5.9.1-050901-generic_5.9.1-050901.202010170731_amd64.deb"
 )
 
 
