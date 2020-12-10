e2e2c8664c9c9faa8dd36aa8f71507cbe4fc2db1
Uri Herrera <hello@uriherrera.com>
2020-12-10 03:41:08 -0600

    add latest lts kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.81/amd64/linux-headers-5.4.81-050481-generic_5.4.81-050481.202012020837_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.81/amd64/linux-headers-5.4.81-050481_5.4.81-050481.202012020837_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.81/amd64/linux-image-unsigned-5.4.81-050481-generic_5.4.81-050481.202012020837_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.81/amd64/linux-modules-5.4.81-050481-generic_5.4.81-050481.202012020837_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.82/amd64/linux-headers-5.4.82-050482-generic_5.4.82-050482.202012080534_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.82/amd64/linux-headers-5.4.82-050482_5.4.82-050482.202012080534_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.82/amd64/linux-image-unsigned-5.4.82-050482-generic_5.4.82-050482.202012080534_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.82/amd64/linux-modules-5.4.82-050482-generic_5.4.82-050482.202012080534_amd64.deb"
 )
 
 
