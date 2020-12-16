bd198c087d51b1dfbc4e4f2e260b033de79e7dcf
Uri Herrera <hello@uriherrera.com>
2020-12-16 17:28:21 -0600

    add latest current kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.12/amd64/linux-headers-5.9.12-050912-generic_5.9.12-050912.202012020835_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.12/amd64/linux-headers-5.9.12-050912_5.9.12-050912.202012020835_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.12/amd64/linux-image-unsigned-5.9.12-050912-generic_5.9.12-050912.202012020835_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.12/amd64/linux-modules-5.9.12-050912-generic_5.9.12-050912.202012020835_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.15/amd64/linux-headers-5.9.15-050915-generic_5.9.15-050915.202012160632_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.15/amd64/linux-headers-5.9.15-050915_5.9.15-050915.202012160632_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.15/amd64/linux-image-unsigned-5.9.15-050915-generic_5.9.15-050915.202012160632_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.15/amd64/linux-modules-5.9.15-050915-generic_5.9.15-050915.202012160632_amd64.deb"
 )
 
 
