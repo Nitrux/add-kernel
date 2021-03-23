88708f21dcfbf809ccdc221105c3a2c47c0280b0
Uri Herrera <hello@uriherrera.com>
2021-03-23 16:44:55 -0600

    add latest 5.4 kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.101/amd64/linux-headers-5.4.101-0504101-generic_5.4.101-0504101.202102261049_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.101/amd64/linux-headers-5.4.101-0504101_5.4.101-0504101.202102261049_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.101/amd64/linux-image-unsigned-5.4.101-0504101-generic_5.4.101-0504101.202102261049_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.101/amd64/linux-modules-5.4.101-0504101-generic_5.4.101-0504101.202102261049_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.107/amd64/linux-headers-5.4.107-0504107-generic_5.4.107-0504107.202103201034_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.107/amd64/linux-headers-5.4.107-0504107_5.4.107-0504107.202103201034_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.107/amd64/linux-image-unsigned-5.4.107-0504107-generic_5.4.107-0504107.202103201034_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.107/amd64/linux-modules-5.4.107-0504107-generic_5.4.107-0504107.202103201034_amd64.deb"
 )
 
 
