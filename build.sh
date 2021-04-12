745f6ab22a205c703b9e9eb883ec38f3ef49fd99
Uri Herrera <hello@uriherrera.com>
2021-04-11 23:16:41 -0500

    try again

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -xe
 
 # Add xanmod repo
 
-echo 'deb http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-kernel.list
-wget -qO - https://dl.xanmod.org/gpg.key | apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
 sudo apt update
 
 # Download files
