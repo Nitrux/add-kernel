e5f21359ff8d5ca14b4d60557214b3605800db7e
Uri Herrera <hello@uriherrera.com>
2021-05-11 06:46:45 -0500

    add latest libre lts kernel

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@
 	cd files
 
 	wget -q \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.10.33-gnu/linux-headers-5.10.33-gnu_5.10.33-gnu-1.0_amd64.deb" \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.10.33-gnu/linux-image-5.10.33-gnu_5.10.33-gnu-1.0_amd64.deb"
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.10.35-gnu/linux-headers-5.10.35-gnu_5.10.35-gnu-1.0_amd64.deb" \
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.10.35-gnu/linux-image-5.10.35-gnu_5.10.35-gnu-1.0_amd64.deb"
 )
 
 
