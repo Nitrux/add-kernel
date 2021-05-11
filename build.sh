468287f661bbd8ae55d23475066444cd55327cf3
Uri Herrera <hello@uriherrera.com>
2021-05-11 06:31:38 -0500

    add latest current kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12/amd64/linux-headers-5.12.0-051200-generic_5.12.0-051200.202104252130_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12/amd64/linux-headers-5.12.0-051200_5.12.0-051200.202104252130_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12/amd64/linux-image-unsigned-5.12.0-051200-generic_5.12.0-051200.202104252130_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12/amd64/linux-modules-5.12.0-051200-generic_5.12.0-051200.202104252130_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.2/amd64/linux-headers-5.12.2-051202-generic_5.12.2-051202.202105071130_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.2/amd64/linux-headers-5.12.2-051202_5.12.2-051202.202105071130_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.2/amd64/linux-image-unsigned-5.12.2-051202-generic_5.12.2-051202.202105071130_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.2/amd64/linux-modules-5.12.2-051202-generic_5.12.2-051202.202105071130_amd64.deb"
 )
 
 
