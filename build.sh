a85ed7902c1719842eb6e0e08ed7e637a2bb0259
Uri Herrera <hello@uriherrera.com>
2021-05-25 18:42:46 -0500

    add latest libre current kernel

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@
 	cd files
 
 	wget -q \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.2-gnu/linux-headers-5.12.2-gnu_5.12.2-gnu-1.0_amd64.deb" \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.2-gnu/linux-image-5.12.2-gnu_5.12.2-gnu-1.0_amd64.deb"
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.6-gnu/linux-headers-5.12.6-gnu_5.12.6-gnu-1.0_amd64.deb" \
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.6-gnu/linux-image-5.12.6-gnu_5.12.6-gnu-1.0_amd64.deb"
 )
 
 
