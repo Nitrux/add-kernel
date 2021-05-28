3725f046e8e898b6703e7968c627c30d9652b263
Uri Herrera <hello@uriherrera.com>
2021-05-28 06:16:47 -0500

    add latest current kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.6/amd64/linux-headers-5.12.6-051206-generic_5.12.6-051206.202105221033_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.6/amd64/linux-headers-5.12.6-051206_5.12.6-051206.202105221033_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.6/amd64/linux-modules-5.12.6-051206-generic_5.12.6-051206.202105221033_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.6/amd64/linux-modules-5.12.6-051206-generic_5.12.6-051206.202105221033_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.7/amd64/linux-headers-5.12.7-051207-generic_5.12.7-051207.202105261234_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.7/amd64/linux-headers-5.12.7-051207_5.12.7-051207.202105261234_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.7/amd64/linux-image-unsigned-5.12.7-051207-generic_5.12.7-051207.202105261234_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.7/amd64/linux-modules-5.12.7-051207-generic_5.12.7-051207.202105261234_amd64.deb"
 )
 
 
