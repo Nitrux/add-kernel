c1745a4649baea8ac1e773c4df83c8d5dc45d271
Uri Herrera <hello@uriherrera.com>
2021-06-17 17:49:04 -0500

    add latest lts kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.35/amd64/linux-headers-5.10.35-051035-generic_5.10.35-051035.202105070631_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.35/amd64/linux-headers-5.10.35-051035_5.10.35-051035.202105070631_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.35/amd64/linux-image-unsigned-5.10.35-051035-generic_5.10.35-051035.202105070631_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.35/amd64/linux-modules-5.10.35-051035-generic_5.10.35-051035.202105070631_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-headers-5.10.36-051036-generic_5.10.36-051036.202105111331_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-headers-5.10.36-051036_5.10.36-051036.202105111331_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-image-unsigned-5.10.36-051036-generic_5.10.36-051036.202105111331_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-modules-5.10.36-051036-generic_5.10.36-051036.202105111331_amd64.deb"
 )
 
 
