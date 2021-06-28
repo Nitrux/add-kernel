193a820cd23e8510d80d81ea6ae500b4c5996bd2
Uri Herrera <hello@uriherrera.com>
2021-06-27 21:34:44 -0500

    add latest 5.4 kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.126/amd64/linux-headers-5.4.126-0504126-generic_5.4.126-0504126.202106160800_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.126/amd64/linux-headers-5.4.126-0504126_5.4.126-0504126.202106160800_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.126/amd64/linux-image-unsigned-5.4.126-0504126-generic_5.4.126-0504126.202106160800_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.126/amd64/linux-modules-5.4.126-0504126-generic_5.4.126-0504126.202106160800_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.128/amd64/linux-headers-5.4.128-0504128-generic_5.4.128-0504128.202106231347_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.128/amd64/linux-headers-5.4.128-0504128_5.4.128-0504128.202106231347_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.128/amd64/linux-image-unsigned-5.4.128-0504128-generic_5.4.128-0504128.202106231347_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.128/amd64/linux-modules-5.4.128-0504128-generic_5.4.128-0504128.202106231347_amd64.deb"
 )
 
 
