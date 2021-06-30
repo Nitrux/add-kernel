abd486bae83b469a985bbdfc4d2b7cd1b2c69d66
Uri Herrera <hello@uriherrera.com>
2021-06-29 19:15:11 -0500

    upload to packagecloud

--- travis-build.sh
+++ travis-build.sh
@@ -10,6 +10,3 @@
 		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.13-gnu/linux-image-5.12.13-gnu_5.12.13-gnu-1.0_amd64.deb"
 )
 
-
-wget -qO nru https://raw.githubusercontent.com/Nitrux/nitrux-repository-util/master/nitrux-repository-util.sh
-bash nru files/*
