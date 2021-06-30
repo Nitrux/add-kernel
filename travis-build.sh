080d7e87400e545944f333f54685994b8e2826fd
Uri Herrera <hello@uriherrera.com>
2021-06-29 19:17:42 -0500

    upload to packagecloud

--- travis-build.sh
+++ travis-build.sh
@@ -12,6 +12,3 @@
 		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-modules-5.10.36-051036-generic_5.10.36-051036.202105111331_amd64.deb"
 )
 
-
-wget -qO nru https://raw.githubusercontent.com/Nitrux/nitrux-repository-util/master/nitrux-repository-util.sh
-bash nru files/*
