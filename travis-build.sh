50afffa06f366e6e8eeeeca9e93c5c75abaed7a1
Uri Herrera <hello@uriherrera.com>
2021-07-02 18:45:03 -0500

    add latest lts kernel

--- travis-build.sh
+++ travis-build.sh
@@ -2,13 +2,13 @@
 
 
 (
-	mkdir files
+	mkdir filesw
 	cd files
 
 	wget -q \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-headers-5.10.36-051036-generic_5.10.36-051036.202105111331_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-headers-5.10.36-051036_5.10.36-051036.202105111331_all.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-image-unsigned-5.10.36-051036-generic_5.10.36-051036.202105111331_amd64.deb" \
-		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.36/amd64/linux-modules-5.10.36-051036-generic_5.10.36-051036.202105111331_amd64.deb"
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.47/amd64/linux-headers-5.10.47-051047-generic_5.10.47-051047.202106301040_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.47/amd64/linux-headers-5.10.47-051047_5.10.47-051047.202106301040_all.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.47/amd64/linux-image-unsigned-5.10.47-051047-generic_5.10.47-051047.202106301040_amd64.deb" \
+		"https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.47/amd64/linux-modules-5.10.47-051047-generic_5.10.47-051047.202106301040_amd64.deb"
 )
 
