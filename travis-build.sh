3cc6b1c8a44a9007309519b46c7b5e24f15b8117
Uri Herrera <hello@uriherrera.com>
2021-07-08 05:22:26 -0500

    add latest libre current kernel

--- travis-build.sh
+++ travis-build.sh
@@ -6,7 +6,7 @@
 	cd files
 
 	wget -q \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.13-gnu/linux-headers-5.12.13-gnu_5.12.13-gnu-1.0_amd64.deb" \
-		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.13-gnu/linux-image-5.12.13-gnu_5.12.13-gnu-1.0_amd64.deb"
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.15-gnu/linux-headers-5.12.15-gnu_5.12.15-gnu-1.0_amd64.deb" \
+		"https://linux-libre.fsfla.org/pub/linux-libre/freesh/pool/main/l/linux-5.12.15-gnu/linux-image-5.12.15-gnu_5.12.15-gnu-1.0_amd64.deb"
 )
 
